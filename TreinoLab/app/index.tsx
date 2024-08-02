c50227bc426987819657dae9fa01600938e15076
PedroHenrick1 <pedrohenrick52@gmail.com>
2024-08-02 10:06:34 -0300

    verificação de usuários

--- TreinoLab/app/index.tsx
+++ TreinoLab/app/index.tsx
@@ -1,7 +1,23 @@
 import { Text, View } from "react-native";
 import Main from "@/components/Main";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
+import { router } from "expo-router";
 
 export default function Index() {
+  useEffect(() => {
+    const checkUser = async () => {
+      const user  = await AsyncStorage.getItem('user');
+      console.log(user);
+      
+      if (user !== null) {
+        router.replace("/Principal");
+      }
+    }
+
+    checkUser();
+  }, []);
+
   return (
     <View>
       <Main/>
